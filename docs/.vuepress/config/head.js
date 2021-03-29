71b206aa838dc1a6dda77ba5196ae2867c168ebd
Alarikshaw <337349132@qq.com>
2021-03-30 00:03:21 +0800

    :crescent_moon::crescent_moon::crescent_moon: install

---
+++ docs/.vuepress/config/head.js
@@ -0,0 +1,13 @@
+// head
+module.exports = [
+  // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+  ['link', { rel: 'icon', href: '/img/td-logo.ico' }], //favicons，资源放在public文件夹
+  [
+    'meta',
+    {
+      name: 'keywords',
+      content: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
+    },
+  ],
+  ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+]
